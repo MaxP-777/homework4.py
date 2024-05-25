3429e58cd57930cd73d7aa4b9b0d95d798a880cb
MaxP-777 <porollomax@mail.ru>
2024-05-25 19:51:06 +0300

    Delete 'homework4.py

--- 'homework4.py
+++
@@ -1,7 +0,0 @@
-my_string = ('Как Вас зовут? ')
-print(len(my_string))
-print(my_string.upper())
-print(my_string.lower())
-print(my_string.replace(my_string, mystring))
-print(my_string[0])
-print(my_string[-1])
